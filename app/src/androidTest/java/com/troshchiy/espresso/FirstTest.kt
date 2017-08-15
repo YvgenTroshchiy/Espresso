2c2044779179f467171f32ba29559ca12bd88711
YevhenTroshciy <yvgen.troshchiy@gmail.com>
2017-08-15 22:29:06 +0300

    Update FirstTest

--- app/src/androidTest/java/com/troshchiy/espresso/FirstTest.kt
+++ app/src/androidTest/java/com/troshchiy/espresso/FirstTest.kt
@@ -1,21 +1,38 @@
 package com.troshchiy.espresso
 
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.typeText
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class FirstTest {
 
-    @Before fun setUp() {
-    }
+    @Rule public var mainActivity: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+    @Rule public var secondactivity: ActivityTestRule<SecondActivity> = ActivityTestRule(SecondActivity::class.java)
 
-    @After fun tearDown() {
-    }
+    @Before fun setUp() {}
+
+    @After fun tearDown() {}
 
     @Test fun passNameToSecondScreen() {
+        val name = "Tra tata"
+        onView(withId(R.id.edt_name)).perform(typeText(name), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.btn_next)).perform(click())
 
+        onView(withId(R.id.tv_name)).check(matches(withText(name)))
     }
 }
