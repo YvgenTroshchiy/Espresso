bfd5b9fc7612f2bc3574e32fe0453685af998060
YevhenTroshciy <yvgen.troshchiy@gmail.com>
2017-08-15 09:44:28 +0300

    Add EXTRA_NAME

--- app/src/main/java/com/troshchiy/espresso/MainActivity.kt
+++ app/src/main/java/com/troshchiy/espresso/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.troshchiy.espresso
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.main_activity.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        btn_next.setOnClickListener {
+            startActivity<SecondActivity>(SecondActivity.EXTRA_NAME to edt_name.text.toString())
+        }
     }
 }
--- app/src/main/java/com/troshchiy/espresso/SecondActivity.kt
+++ app/src/main/java/com/troshchiy/espresso/SecondActivity.kt
@@ -2,11 +2,21 @@ package com.troshchiy.espresso
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.second_activity.*
 
 class SecondActivity : AppCompatActivity() {
+
+    companion object {
+        val EXTRA_NAME = "extra_name"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.second_activity)
+
+        intent?.let {
+            val name = it.getStringExtra(EXTRA_NAME)
+            tv_name.text = name
+        }
     }
 }
