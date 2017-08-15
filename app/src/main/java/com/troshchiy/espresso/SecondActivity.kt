fb6c77b80fc222b5822f11e9d7661e33c3423980
YevhenTroshciy <yvgen.troshchiy@gmail.com>
2017-08-15 09:17:36 +0300

    Add SecondActivity

--- app/src/main/java/com/troshchiy/espresso/SecondActivity.kt
+++ app/src/main/java/com/troshchiy/espresso/SecondActivity.kt
@@ -1,12 +1,12 @@
 package com.troshchiy.espresso
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.second_activity)
     }
 }
