d0d468483930f32dc22fcf341b59a358f0efbcc6
Yvgent Troshchiy <yvgen.troshchiy@gmail.com>
2018-03-20 23:39:30 +0200

    Add MainActivityTest

---
+++ app/src/androidTest/java/com/troshchiy/espresso/MainActivityTest.kt
@@ -0,0 +1,29 @@
+package com.troshchiy.espresso
+
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+@Suppress("IllegalIdentifier") // To be able surround methods name with `
+class MainActivityTest {
+
+    @Rule @JvmField val mainActivity = ActivityTestRule(MainActivity::class.java)
+    @Rule @JvmField val secondActivity = ActivityTestRule(SecondActivity::class.java)
+
+    @Test fun shouldPassNameToTheSecondScreen() {
+        val name = "Name to pass"
+        Espresso.onView(ViewMatchers.withId(R.id.edt_name)).perform(ViewActions.typeText(name), ViewActions.closeSoftKeyboard())
+        Espresso.onView(ViewMatchers.withId(R.id.btn_next)).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.tv_name)).check(ViewAssertions.matches(ViewMatchers.withText(name)))
+    }
+}
