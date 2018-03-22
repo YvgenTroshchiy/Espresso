a189e20c70f924ca493eb81499b82a94d7ed19ea
Yvgent Troshchiy <yvgen.troshchiy@gmail.com>
2018-03-22 12:57:01 +0200

    Add touchDownAndUp event

--- app/src/androidTest/java/com/troshchiy/espresso/MainActivityTest.kt
+++ app/src/androidTest/java/com/troshchiy/espresso/MainActivityTest.kt
@@ -1,11 +1,16 @@
 package com.troshchiy.espresso
 
-import android.support.test.espresso.action.ViewActions.*
+import android.graphics.Rect
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.closeSoftKeyboard
+import android.support.test.espresso.action.ViewActions.typeText
 import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import android.widget.Button
 import com.troshchiy.espresso.ui.MainActivity
 import com.troshchiy.espresso.ui.SecondActivity
 import org.junit.Rule
@@ -16,14 +21,24 @@ import org.junit.runner.RunWith
 @LargeTest
 class MainActivityTest {
 
-    @Rule @JvmField val mainActivity = ActivityTestRule(MainActivity::class.java)
-    @Rule @JvmField val secondActivity = ActivityTestRule(SecondActivity::class.java)
+    @Rule @JvmField val mainActivityRule = ActivityTestRule(MainActivity::class.java)
+    @Rule @JvmField val secondActivityRule = ActivityTestRule(SecondActivity::class.java)
 
     @Test fun shouldPassNameToTheSecondScreen() {
         val name = "Name to pass"
 
         R.id.edt_name.perform2(typeText(name), closeSoftKeyboard())
-        R.id.btn_next perform click()
+
+        val btnNext = mainActivityRule.activity.findViewById<Button>(R.id.btn_next)
+
+        onView(withId(R.id.constraintLayout)).perform(
+                touchDownAndUp(
+                        (btnNext.left + btnNext.width / 2).toFloat(),
+                        (btnNext.top + btnNext.height / 2).toFloat()
+                )
+        )
+
+//        R.id.btn_next perform click()
 
         R.id.tv_name check isDisplayed
         R.id.tv_name check matches(withText(name))
--- app/src/androidTest/java/com/troshchiy/espresso/extensions.kt
+++ app/src/androidTest/java/com/troshchiy/espresso/extensions.kt
@@ -1,16 +1,43 @@
 package com.troshchiy.espresso
 
-import android.support.test.espresso.Espresso
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.UiController
 import android.support.test.espresso.ViewAction
 import android.support.test.espresso.ViewAssertion
 import android.support.test.espresso.ViewInteraction
-import android.support.test.espresso.assertion.ViewAssertions
-import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.action.MotionEvents.sendDown
+import android.support.test.espresso.action.MotionEvents.sendUp
+import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.view.View
+import org.hamcrest.Matcher
 
-val isDisplayed: ViewAssertion = ViewAssertions.matches(isDisplayed())
 
-infix fun Int.perform(action: ViewAction): ViewInteraction = Espresso.onView(ViewMatchers.withId(this)).perform(action)
-fun Int.perform2(action1: ViewAction, action2: ViewAction): ViewInteraction = Espresso.onView(ViewMatchers.withId(this)).perform(action1, action2)
+val isDisplayed: ViewAssertion = matches(isDisplayed())
 
-infix fun Int.check(action: ViewAssertion): ViewInteraction = Espresso.onView(ViewMatchers.withId(this)).check(action)
+infix fun Int.perform(action: ViewAction): ViewInteraction = onView(withId(this)).perform(action)
+fun Int.perform2(action1: ViewAction, action2: ViewAction): ViewInteraction = onView(withId(this)).perform(action1, action2)
+
+infix fun Int.check(action: ViewAssertion): ViewInteraction = onView(withId(this)).check(action)
+
+fun touchDownAndUp(x: Float, y: Float): ViewAction =
+        object : ViewAction {
+            override fun getConstraints(): Matcher<View> = isDisplayed()
+            override fun getDescription() = "Send touch down and up event"
+
+            override fun perform(uiController: UiController, view: View) {
+                // Get view absolute position
+                val locationOnScreen = IntArray(2)
+                view.getLocationOnScreen(locationOnScreen)
+
+                // Offset coordinates by view position
+                val coordinates = floatArrayOf(x + locationOnScreen[0], y + locationOnScreen[1])
+                val precision = floatArrayOf(1f, 1f)
+
+                // Send down event, pause, and send up
+                val down = sendDown(uiController, coordinates, precision).down
+                uiController.loopMainThreadForAtLeast(200)
+                sendUp(uiController, down, coordinates)
+            }
+        }
